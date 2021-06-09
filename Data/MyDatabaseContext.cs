6db8d94ab1566c0c72b7b4af2816004f0534d2b9
Andrew Batchelor <andrew.batchelor@inter.ikea.com>
2021-06-09 19:10:32 +0200

    enable aad msi

--- Data/MyDatabaseContext.cs
+++ Data/MyDatabaseContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace DotNetCoreSqlDb.Models
@@ -11,6 +12,10 @@ namespace DotNetCoreSqlDb.Models
         public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
             : base(options)
         {
+            // feature/aad-msi
+            // https://docs.microsoft.com/en-us/azure/app-service/app-service-web-tutorial-connect-msi#modify-aspnet-core
+            var connection = (SqlConnection)Database.GetDbConnection();
+            connection.AccessToken = (new Microsoft.Azure.Services.AppAuthentication.AzureServiceTokenProvider()).GetAccessTokenAsync("https://database.windows.net/").Result;
         }
 
         public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; }
